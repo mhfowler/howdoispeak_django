385ee5d057b683abd619fae561d912ddd17230ec
Max Fowler <max_fowler@brown.edu>
2014-04-30 18:09:23 -0400

    example ajax action

--- hdis/views.py
+++ hdis/views.py
@@ -4,6 +4,8 @@ import json
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from hdis.models import HowDoISpeakUser
+from settings.common import getHDISBucket
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie
 
 
 def redirect(request, page='/home'):
@@ -25,9 +27,36 @@ def html_response(request, template, data_dict=None):
 def home(request):
     return  html_response(request=request, template="landing.html")
 
+@ensure_csrf_cookie
 def user_page(request, user_pin):
     hdis_user = HowDoISpeakUser.objects.get(user_pin=user_pin)
     return  html_response(request=request, template="user_page.html", data_dict={"user_pin":user_pin})
+
+######### ajax actions ###################
+@csrf_exempt
+def ajaxAction(request):
+    action = request.POST["action"]
+    action_map = {
+        "getWordFreq":getWordFreq
+    }
+    return action_map[action](request)
+
+
+def getWordFreq(request):
+    user_pin = request.POST["user_pin"]
+    word = request.POST["word"]
+    hdis_user = HowDoISpeakUser.objects.get(user_pin=user_pin)
+    bucket = getHDISBucket()
+    freq_key_name = hdis_user.getFreqKeyName()
+    freq_key = bucket.get_key(freq_key_name)
+    freq_json = freq_key.get_contents_as_string()
+    freq_dict = json.loads(freq_json)["total_freq"]
+    word_freq = freq_dict.setdefault(word, 0)
+    to_return = {"freq":word_freq}
+    return json_response(to_return)
+
+
+
 
 
 
--- urls.py
+++ urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
-from hdis.views import viewWrapper, home, redirect, user_page
+from hdis.views import viewWrapper, home, redirect, user_page, ajaxAction
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
 
    (r'^home/$', viewWrapper(home)),
+   (r'^ajax/$', viewWrapper(ajaxAction)),
    (r'^user/(\S+)/$', viewWrapper(user_page)),
    (r'.*$',  redirect, {'page':"/home/"}),
 
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
